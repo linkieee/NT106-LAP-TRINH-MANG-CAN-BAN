e4014addd4f818a1c2e14fdd6d4f9e0c1fe9b3e7
linkieee <geniegialinh@gmail.com>
2024-05-15 15:24:06 +0700

    add project lab4

---
+++ LAB4/Working with Web Server/Working with Web Server/Bai1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Working_with_Web_Server
+{
+    public partial class Bai1 : Form
+    {
+        public Bai1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LAB4/Working with Web Server/Working with Web Server/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Working_with_Web_Server
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Bai1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Bai1 b1 = new Bai1();
+            b1.ShowDialog();
+            b1 = null;
+            this.Show();
+        }
+    }
+}
