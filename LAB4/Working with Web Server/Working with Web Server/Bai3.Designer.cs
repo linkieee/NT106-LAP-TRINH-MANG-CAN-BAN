39b6b2a62ec6e135cb28f12d97b036b769a81dfc
linkieee <geniegialinh@gmail.com>
2024-05-20 22:17:02 +0700

    done bai3 lab4

---
+++ LAB4/Working with Web Server/Working with Web Server/Bai3.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Working_with_Web_Server
+{
+    partial class Bai3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbSaveFile = new System.Windows.Forms.RichTextBox();
+            this.tbHTML = new System.Windows.Forms.RichTextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.tbURL = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // tbSaveFile
+            // 
+            this.tbSaveFile.Location = new System.Drawing.Point(16, 67);
+            this.tbSaveFile.Name = "tbSaveFile";
+            this.tbSaveFile.Size = new System.Drawing.Size(630, 39);
+            this.tbSaveFile.TabIndex = 13;
+            this.tbSaveFile.Text = "";
+            // 
+            // tbHTML
+            // 
+            this.tbHTML.Location = new System.Drawing.Point(16, 121);
+            this.tbHTML.Name = "tbHTML";
+            this.tbHTML.Size = new System.Drawing.Size(767, 316);
+            this.tbHTML.TabIndex = 12;
+            this.tbHTML.Text = "";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(668, 14);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(116, 38);
+            this.btnDownload.TabIndex = 11;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // tbURL
+            // 
+            this.tbURL.Location = new System.Drawing.Point(16, 14);
+            this.tbURL.Name = "tbURL";
+            this.tbURL.Size = new System.Drawing.Size(630, 39);
+            this.tbURL.TabIndex = 10;
+            this.tbURL.Text = "";
+            // 
+            // Bai3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbSaveFile);
+            this.Controls.Add(this.tbHTML);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.tbURL);
+            this.Name = "Bai3";
+            this.Text = "Bai3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox tbSaveFile;
+        private System.Windows.Forms.RichTextBox tbHTML;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.RichTextBox tbURL;
+    }
+}
---
+++ LAB4/Working with Web Server/Working with Web Server/Bai3.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Working_with_Web_Server
+{
+    public partial class Bai3 : Form
+    {
+        public Bai3()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string URL = tbURL.Text;
+            string fileURL = tbSaveFile.Text;
+            string WebHTML = "";
+            WebHTML = getHTML(tbURL.Text);
+            tbHTML.Text = WebHTML;
+            WebClient myClient = new WebClient();
+            Stream response = myClient.OpenRead(URL);
+            myClient.DownloadFile(URL, fileURL);
+        }
+
+        private string getHTML(string szURL)
+        {
+            WebRequest request = WebRequest.Create(szURL);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            response.Close();
+            return responseFromServer;
+        }
+
+    }
+}
